6dfece6d4a78526f86952bdbcd8c81de249b3e4d
nghiahuynhtv01042002 <nghiahuynhtv01042002@gmail.com>
2024-12-15 19:14:25 +0700

    update BALL class

--- Ponggame/main.cpp
+++ Ponggame/main.cpp
@@ -25,19 +25,29 @@ public:
         this->ySpeed = ySpeed;
     }
     float getX_Pos(){
-        return this->xPosition;
+        return xPosition;
     }
     float getY_Pos(){
-        return this->yPosition;
+        return yPosition;
     }
     int getX_Speed(){
-        return this->xSpeed;
+        return xSpeed;
     }
     int getY_Speed(){
-        return this->ySpeed;
+        return ySpeed;
     }
-    void Draw(float xPosition, float yPosition ,float radius){
+    void Draw(){
         DrawCircle(xPosition, yPosition, radius, WHITE);
+    }
+    void UpdatePostion(){
+        this->xPosition += this->xSpeed;
+        this->yPosition += this->ySpeed;
+        if((int)(this->yPosition + this->radius) >= GetScreenHeight() || this->yPosition -  this->radius <=0){
+            this->ySpeed *=-1;
+        }
+        if((int)(this->xPosition + this->radius) >= GetScreenWidth() || this->xPosition - this->radius <=0){
+            this->xSpeed *=-1;
+        }
     }
 
 };
@@ -48,11 +58,13 @@ int main () {
     InitWindow(screen_width, screen_height, titile);
     SetTargetFPS(60);
     Ball ball(screen_width/2, screen_height/2, 20.00);
+    ball.setSpeed(7 ,7);
     while(!WindowShouldClose()){
         BeginDrawing();
-        // DrawCircle(screen_width/2, screen_height/2, 20.00, WHITE);
-        ball.Draw(screen_width/2, screen_height/2, 20.00);
-
+        ball.UpdatePostion();
+        // Drawing  stuff
+        ClearBackground(BLACK);
+        ball.Draw();
         DrawRectangle(10,screen_height/2-60,25,120,WHITE);
         DrawRectangle(screen_width -35 ,screen_height/2-60,25,120,WHITE);
         DrawLine(screen_width/2, 0 ,screen_width/2, screen_height ,WHITE);
