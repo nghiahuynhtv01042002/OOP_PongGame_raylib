5b5b79ee94d54dd32e1ca9aca038947b49902228
nghiahuynhtv01042002 <nghiahuynhtv01042002@gmail.com>
2024-12-15 17:49:03 +0700

    create game loop

--- Ponggame/main.cpp
+++ Ponggame/main.cpp
@@ -3,11 +3,16 @@
 
 
 int main () {
-    const int screen_width = 500;
+    const int screen_width = 1280;
     const int screen_height = 800;
     const char* titile = "Pong Game made by TXN";
     InitWindow(screen_width, screen_height, titile);
-    
+    SetTargetFPS(60);
+    while(!WindowShouldClose()){
+        BeginDrawing();
+
+        EndDrawing();
+    }
     CloseWindow();
     return 0;
 }
